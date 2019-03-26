499fd228ee1091029ad7ca75f114eaa4353ba027
Mark Okafor <okaformark@gmail.com>
2019-03-25 20:30:23 -0500

    form prints and clears correctly

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log('hi');
+const addButton = document.getElementById('addButton');
+const inputIngredient = document.getElementById('inputIngredient');
+
+const printToDom = (divId, textToPrint) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML += textToPrint;
+};
+
+const addIngredient = (e) =>{
+    e.preventDefault();
+    const inputText = inputIngredient.value;
+    printToDom('ingredient-container', inputText);
+    inputIngredient.value = '';
+}
+const eventListener = () => {
+    addButton.addEventListener ('click', addIngredient);
+};
+
+const init = () => {
+    eventListener();
+};
+init();
